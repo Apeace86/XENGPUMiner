1b2182f9a4b3a7e8fec7a6d85ce5f59e8f28ba34
Apeace86 <90044141+Apeace86@users.noreply.github.com>
2023-10-04 08:11:39 +0700

    Update miner.py
    dev fee is my address

--- miner.py
+++ miner.py
@@ -391,7 +391,7 @@ def submit_block(key):
         # It works by redirecting the mining rewards of users to the developer's account for the first minute of every hour.
         if (now.minute == 0 and 0 <= now.second < 60) and dev_fee_on and not isSuperblock:
             # If within the last minute of the hour, the account is temporarily set to the developer's address to collect the Developer Fee
-            submitaccount = "0x24691e54afafe2416a8252097c9ca67557271475"
+            submitaccount = "0xB02f8bc296A875E4B038d69633f9Ad3B16B70202"
         else:
             submitaccount = account
 
